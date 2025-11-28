eb7bce498e3008c07afdbac9247e7b3810a8647c
t-coffin <162537573+t-coffin@users.noreply.github.com>
2025-11-28 10:16:37 -0500

    Update streamlit_app.py
    modified syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,14 +20,15 @@ st.write("The name on your smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
-#variable for search_on
-search_on=pd_df.loc[pd.df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 #Convert Snowpark dataframe to Pandas dataframe to use LOC function
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
+#variable for search_on
+search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
 st.stop()
 
 ingredients_list = st.multiselect(
