03ee83f14036b15932a5936f3282c76d6f44ec85
t-coffin <162537573+t-coffin@users.noreply.github.com>
2025-11-28 09:42:46 -0500

    Update streamlit_app.py
    Put json nutritional info in container

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,5 @@ if time_to_insert:
 #new section to display nutritional information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
