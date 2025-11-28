aa662c93a1254292d06d76f90bd238c373502453
t-coffin <162537573+t-coffin@users.noreply.github.com>
2025-11-28 09:39:49 -0500

    Update streamlit_app.py
    Updated to display json for nutritional api call

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,4 @@ if time_to_insert:
 #new section to display nutritional information
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
