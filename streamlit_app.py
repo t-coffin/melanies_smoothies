a0971ae336c8389ced67e69728266427eda67282
t-coffin <162537573+t-coffin@users.noreply.github.com>
2025-11-28 10:17:38 -0500

    Update streamlit_app.py
    commented out stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,7 @@ pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 #variable for search_on
 search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-st.stop()
+#st.stop()
 
 ingredients_list = st.multiselect(
 'Choose up to 5 ingredients:'
